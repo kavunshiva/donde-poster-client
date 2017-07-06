9d5f28660f1a53b29df3d162bf38bf4e05e908fe
kavunshiva <kavunshiva@gmail.com>
2017-07-06 15:36:06 -0400

    make headers a callback function in adapter to refresh on every request

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -1,32 +1,33 @@
 const baseUrl = `http://localhost:3000/api/v1`
 
-const headers = {
+const headers = () => ({
     'content-type': 'application/json',
     'accept': 'application/json',
     'Authorization': localStorage.getItem('jwt')
-}
+})
 
 export class AuthAdapter {
   static login(params){
     return fetch(`${baseUrl}/auth`, {
       method: 'post',
-      headers: headers,
+      headers: headers(),
       body: JSON.stringify(params)
     }).then(res => res.json())
   }
 
   static currentDevice(){
     return fetch(`${baseUrl}/current_device`, {
-      headers: headers
+      headers: headers()
     }).then(res => res.json())
   }
 }
 
 export class PositionsAdapter {
   static create(position){
+    debugger
     return fetch(`${baseUrl}/positions`, {
         method: 'post',
-        headers: headers,
+        headers: headers(),
         body: JSON.stringify({
           position: position
         })
